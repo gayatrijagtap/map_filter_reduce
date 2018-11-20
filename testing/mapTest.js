ac90c77d3b192b9c8c25d6733a9d861a6dc81e02
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-20 14:05:23 +0530

    more tests added in mapTest.

--- testing/mapTest.js
+++ testing/mapTest.js
@@ -5,5 +5,9 @@ const { cube } = require('../utilityFun.js');
 
 assert.deepEqual(map([1,2,3,4,5],square),[1,4,9,16,25]);
 assert.deepEqual(map([2,4,6],square),[4,16,36]);
+assert.deepEqual(map([-1],square),[1]);
+assert.deepEqual(map([],square),[]);
 assert.deepEqual(map([1,2,3],cube),[1,8,27]);
 assert.deepEqual(map([1],cube),[1]);
+assert.deepEqual(map([-1],cube),[-1]);
+assert.deepEqual(map([],cube),[]);
