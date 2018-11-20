1fd6f3903f35a10e2087da3fd0232e90f5573ce3
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-20 14:08:38 +0530

    more tests added in filterTest.

--- testing/filterTest.js
+++ testing/filterTest.js
@@ -5,5 +5,10 @@ const { isOdd } = require('../utilityFun.js');
 
 assert.deepEqual(filter([1,2,3,4,5],isEven),[2,4]);
 assert.deepEqual(filter([1,2,3],isEven),[2]);
+assert.deepEqual(filter([1,3,5],isEven),[]);
+assert.deepEqual(filter([],isEven),[]);
+
 assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
 assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
+assert.deepEqual(filter([],isOdd),[]);
+assert.deepEqual(filter([2,4],isOdd),[]);
