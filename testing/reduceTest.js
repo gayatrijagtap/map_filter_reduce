49b9ba1ac687c3e8b98ddff2f611c86f643a8029
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-20 14:42:30 +0530

    more tests added in reduceTest.

--- testing/reduceTest.js
+++ testing/reduceTest.js
@@ -5,5 +5,9 @@ const { max } = require('../utilityFun.js');
 
 assert.equal(reduce([1,2,3,4,5],add),15);
 assert.equal(reduce([1,2,3,4,5],add,2),17);
+assert.equal(reduce([1],add,2),3);
+assert.equal(reduce([1,2],add),3);
 assert.equal(reduce([1,2,3,4,5],max),5);
 assert.equal(reduce([1,2,3,4,5],max,9),9);
+assert.equal(reduce([1,2],max,9),9);
+assert.equal(reduce([1,2],max),2);
