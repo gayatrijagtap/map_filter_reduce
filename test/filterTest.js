22142c9c85cdfb1b05dc9996b398a47173e0b2e9
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-21 15:54:36 +0530

    added test for all truthy filters

--- test/filterTest.js
+++ test/filterTest.js
@@ -10,12 +10,20 @@ describe( 'filter' , function() {
     assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
     assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
   })
+
   it( 'should return empty array when empty array is given as an input' , function() {
     assert.deepEqual(filter([],isEven),[]);
     assert.deepEqual(filter([],isOdd),[]);
   });
+
   it( 'should return empty array for all falsie filters' , function() {
     assert.deepEqual(filter([1,3,5],isEven),[]);
     assert.deepEqual(filter([2,4],isOdd),[]);
   });
+
+  it( 'should return all the elements when all the filters are truthy' , function() {
+    assert.deepEqual(filter([2,4,6],isEven),[2,4,6]);
+    assert.deepEqual(filter([1,3,5],isOdd),[1,3,5]);
+  });
+
 })
