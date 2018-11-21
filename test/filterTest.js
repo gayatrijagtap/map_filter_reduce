d15bbbb99ec8718b4a1168f9292d62e3184d0849
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-21 14:38:13 +0530

    Refactored: made tests more descriptive

--- test/filterTest.js
+++ test/filterTest.js
@@ -4,25 +4,18 @@ const { isEven } = require('../utilityFun.js');
 const { isOdd } = require('../utilityFun.js');
 
 describe( 'filter' , function() {
-  describe( 'isEven' , function () {
-    it( 'should give an array of even numbers' , function() {
-      assert.deepEqual(filter([1,2,3,4,5],isEven),[2,4]);
-      assert.deepEqual(filter([1,2,3],isEven),[2]);
-    })
-    it( 'should work for empty output' , function() {
-      assert.deepEqual(filter([1,3,5],isEven),[]);
-      assert.deepEqual(filter([],isEven),[]);
-    });
+  it( 'should filter for array with multiple elements' , function() {
+    assert.deepEqual(filter([1,2,3,4,5],isEven),[2,4]);
+    assert.deepEqual(filter([1,2,3],isEven),[2]);
+    assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
+    assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
   })
-
-  describe( 'isOdd' , function() {
-    it( 'should give an array of odd numbers' , function() {
-      assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
-      assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
-    });
-    it( 'should work for empty output' , function() {
-      assert.deepEqual(filter([],isOdd),[]);
-      assert.deepEqual(filter([2,4],isOdd),[]);
-    });
-  })
+  it( 'should return empty array when empty array is given as an input' , function() {
+    assert.deepEqual(filter([],isEven),[]);
+    assert.deepEqual(filter([],isOdd),[]);
+  });
+  it( 'should return empty array for all falsie filters' , function() {
+    assert.deepEqual(filter([1,3,5],isEven),[]);
+    assert.deepEqual(filter([2,4],isOdd),[]);
+  });
 })
--- test/mapTest.js
+++ test/mapTest.js
@@ -4,29 +4,22 @@ const { square } = require('../utilityFun.js');
 const { cube } = require('../utilityFun.js');
 
 describe( 'map' , function() {
-  describe( 'square' , function () {
-    it( 'should square the positive integers' , function() {
-      assert.deepEqual(map([1,2,3,4,5],square),[1,4,9,16,25]);
-      assert.deepEqual(map([2,4,6],square),[4,16,36]);
-    });
-    it( 'should square the negative integers' , function () {
-      assert.deepEqual(map([-1],square),[1]);
-    });
-    it( 'should work for the empty array' , function () {
-      assert.deepEqual(map([],square),[]);
-    });
-  })
 
-  describe ( 'cube' , function () {
-    it( 'should cube the positive integers' , function() {
-      assert.deepEqual(map([1,2,3],cube),[1,8,27]);
-      assert.deepEqual(map([1],cube),[1]);
-    });
-    it( 'should cube the negative integers' , function() {
-      assert.deepEqual(map([-1],cube),[-1]);
-    });
-    it( 'should work for the empty array' , function() {
-      assert.deepEqual(map([],cube),[]);
-    });
-  })
+  it( 'should map positive integers' , function() {
+    assert.deepEqual(map([1,2,3,4,5],square),[1,4,9,16,25]);
+    assert.deepEqual(map([2,4,6],square),[4,16,36]);
+    assert.deepEqual(map([1,2,3],cube),[1,8,27]);
+    assert.deepEqual(map([1],cube),[1]);
+  });
+
+  it( 'should map negative integers' , function () {
+    assert.deepEqual(map([-1],square),[1]);
+    assert.deepEqual(map([-1],cube),[-1]);
+  });
+
+  it( 'should give an empty array when empty array is given as an input' , function () {
+    assert.deepEqual(map([],square),[]);
+    assert.deepEqual(map([],cube),[]);
+  });
+
 })
