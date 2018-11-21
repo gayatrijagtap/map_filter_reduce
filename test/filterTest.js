6a459ee79929d4bbab22843d0df192a05c82e087
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2018-11-21 09:39:10 +0530

    Refactored: made tests more descriptive

--- test/filterTest.js
+++ test/filterTest.js
@@ -3,24 +3,26 @@ const { filter } = require('../library.js');
 const { isEven } = require('../utilityFun.js');
 const { isOdd } = require('../utilityFun.js');
 
-describe( 'isEven' , function () {
-  it( 'should give an array of even numbers' , function() {
-    assert.deepEqual(filter([1,2,3,4,5],isEven),[2,4]);
-    assert.deepEqual(filter([1,2,3],isEven),[2]);
+describe( 'filter' , function() {
+  describe( 'isEven' , function () {
+    it( 'should give an array of even numbers' , function() {
+      assert.deepEqual(filter([1,2,3,4,5],isEven),[2,4]);
+      assert.deepEqual(filter([1,2,3],isEven),[2]);
+    })
+    it( 'should work for empty output' , function() {
+      assert.deepEqual(filter([1,3,5],isEven),[]);
+      assert.deepEqual(filter([],isEven),[]);
+    });
   })
-  it( 'should work for empty output' , function() {
-    assert.deepEqual(filter([1,3,5],isEven),[]);
-    assert.deepEqual(filter([],isEven),[]);
-  });
-})
 
-describe( 'isOdd' , function() {
-  it( 'should give an array of odd numbers' , function() {
-    assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
-    assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
-  });
-  it( 'should work for empty output' , function() {
-    assert.deepEqual(filter([],isOdd),[]);
-    assert.deepEqual(filter([2,4],isOdd),[]);
-  });
+  describe( 'isOdd' , function() {
+    it( 'should give an array of odd numbers' , function() {
+      assert.deepEqual(filter([1,2,3],isOdd),[1,3]);
+      assert.deepEqual(filter([1,2,3,4,5],isOdd),[1,3,5]);
+    });
+    it( 'should work for empty output' , function() {
+      assert.deepEqual(filter([],isOdd),[]);
+      assert.deepEqual(filter([2,4],isOdd),[]);
+    });
+  })
 })
